16f41f2ea91de9ef2f683e4ef2009a40a3274c26
Guilherme Alves Silveira <guilherme_alves_silve@hotmail.com>
2021-04-03 23:28:56 -0300

    Finish study

--- lab-6/initial-oidc-client/src/main/java/com/example/oidc/client/api/UserInfoRestController.java
+++ lab-6/initial-oidc-client/src/main/java/com/example/oidc/client/api/UserInfoRestController.java
@@ -1,5 +1,7 @@
 package com.example.oidc.client.api;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -10,7 +12,7 @@ import java.util.Map;
 public class UserInfoRestController {
 
   @GetMapping("/userinfo")
-  Mono<Map<String, Object>> userInfo() {
-    return Mono.empty();
+  Mono<Map<String, Object>> userInfo(@AuthenticationPrincipal OAuth2User oauth2User) {
+    return Mono.just(oauth2User.getAttributes());
   }
 }
